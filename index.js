3c67af08401dd045e4a35a9f6fdb8cdd66c1fe38
Terrill <t.thorne96@yahoo.com>
2018-06-04 18:13:03 -0400

    Second commit

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there'});
-});
-
-app.listen(5000);
+// app.get('/', (req, res) => {
+//   res.send({ hi: 'there'});
+// });
+//
+// app.listen(PORT);
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
