97977b8c0a4c3220c3b79011854107fb56619c34
Terrill <t.thorne96@yahoo.com>
2018-06-05 15:02:22 -0400

    removed fav icon from index

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const express = require('express');
+const favicon = require('express-favicon');
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-// app.get('/', (req, res) => {
-//   res.send({ hi: 'there'});
-// });
-//
-// app.listen(PORT);
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get('/', (req, res) => {
+  res.send({ hi: 'there'});
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
