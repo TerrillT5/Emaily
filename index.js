b57cf6bd7314f32a4eb0c082e832222929416aa7
Terrill <t.thorne96@yahoo.com>
2018-06-05 22:13:48 -0400

    Added google strategy

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const favicon = require('express-favicon');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'By buddy'});
-});
+
+//client id 881656482840-e9o23lgru7u1b5o3bk56002qgcs5k922.apps.googleusercontent.com
+// client secret Uro5xHN7FZLor6rQUNmpIT85
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
