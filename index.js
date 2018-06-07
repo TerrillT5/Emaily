aa6cd668242e5358e419f5dc1bdf3941c84854fc
Terrill <t.thorne96@yahoo.com>
2018-06-06 21:51:56 -0400

    Added passport keys

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+}, (accessToken) => {
+  console.log(accessToken);
+   })
+);
 
-//client id 881656482840-e9o23lgru7u1b5o3bk56002qgcs5k922.apps.googleusercontent.com
-// client secret Uro5xHN7FZLor6rQUNmpIT85
-passport.use(new GoogleStrategy());
+// get method of request
+// path to handle then whats executed when a request comes in
+app.get('/auth/google',passport.authenticate('google', {
+  scope: ['profile', 'email']
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
