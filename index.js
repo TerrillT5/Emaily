e3ef4fcb9c8037c034e3caa4a9bbb8be3a184517
Terrill <t.thorne96@yahoo.com>
2018-06-06 22:31:26 -0400

    commented out web link

--- index.js
+++ index.js
@@ -29,4 +29,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
 
-https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth%2Fgoogle%2Fcallback&scope=profile%20email&client_id=881656482840-e9o23lgru7u1b5o3bk56002qgcs5k922.apps.googleusercontent.com
+// https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth%2Fgoogle%2Fcallback&scope=profile%20email&client_id=881656482840-e9o23lgru7u1b5o3bk56002qgcs5k922.apps.googleusercontent.com
