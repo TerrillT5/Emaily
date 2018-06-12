c14c27e20c8190a5670a41549755487b6e2695cc
Terrill <t.thorne96@yahoo.com>
2018-06-11 22:49:23 -0400

    Changed state to null

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,7 +1,10 @@
+import { FETCH_USER } from '../actions/types';
 // Wiring up redux
-export default function (state = {}, action) {
+export default function (state = null, action) {
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false
     default:
-    return state;
+      return state;
   }
 }
