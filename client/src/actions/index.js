007a6580589526abc9f34f813594dc68baf8c8fe
Terrill <t.thorne96@yahoo.com>
2018-06-15 17:21:25 -0400

    Added Semicolon

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -8,7 +8,7 @@ import { FETCH_USER } from './types';
   }
 
 export const handleToken = (token) => async dispatch => {
-  const res = await axios.post('/api/stripe', token)
+  const res = await axios.post('/api/stripe', token);
 
   dispatch({ type: FETCH_USER, payload: res.data });
 }
