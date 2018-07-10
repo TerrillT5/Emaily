72b71ee2096d42dc12ba77b73280709cabf4a712
TerrillT5 <t.thorne96@yahoo.com>
2018-07-10 18:26:11 -0400

    new component that will handle information for survey input

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import _ from 'lodash';
+import { reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField';
+
+
+const FIELDS = [
+  { label: "Survey Title", name: "title" },
+  { label: "Subject Line", name: "subject" },
+  { label: "Email Body", name: "body" },
+  { label: "Email List", name: "emails" }
+]
+
+class SurveyForm extends Component {
+  renderFields = () => {
+    return _.map(FIELDS, ({ label, name }) => {
+      return (
+        <Field key={name} component={SurveyField} type="text" label={label} name={name} />
+      );
+    })
+
+}
+
+  render() {
+    return (
+      <div>
+      <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          {this.renderFields()}
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+
+}
+
+export default reduxForm({form: 'surveyForm'})(SurveyForm);
