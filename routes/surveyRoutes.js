a4175117bfe0a197d8af1296a6fca94f09a414ca
TerrillT5 <t.thorne96@yahoo.com>
2018-07-10 14:46:02 -0400

    Added async code to handle requests
    The requests will send and the survey will save along with the user updated credits

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -8,7 +8,7 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 // https://github.com/farhantahir/emaily-app-mern-basics/blob/master/routes/surveys.js
 
 module.exports = app => {
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
   const { title, subject, body, recipients } = req.body;
 
   const survey = new Survey({
@@ -18,9 +18,20 @@ module.exports = app => {
      recipients: recipients.split(',').map(recipient => ({ email: recipient.trim() })),
      _user: req.user.id,
      dateSent: Date.now()
-     })
+   });
      // Place to send an email
      const mailer = new Mailer(survey, surveyTemplate(survey));
-     mailer.send();
+
+     try {
+       await mailer.send();
+       await survey.save();
+        req.user.credits -= 1;
+        const user = await req.user.save();
+
+       res.send(user);
+     } catch (err) {
+       res.status(422).send(err);
+     }
    });
+
  }
--- services/emailTemplates/Mailer.js
+++ services/emailTemplates/Mailer.js
@@ -42,16 +42,16 @@ class Mailer extends helper.Mail {
 
    async send() {
      try {
-     const request = this.sgApi.emptyRequest({
+      const request = this.sgApi.emptyRequest({
        method: 'POST',
        path: '/v3/mail/send',
        body: this.toJSON()
      });
 
-    const response = this.sgApi.API(request);
+    const response = await this.sgApi.API(request);
       return response
    } catch (err) {
-     console.log('Heres the error',err)
+     console.log('Heres the error', err)
    }
  }
 
