b5ee35243dd475beb587d9a03687c3fa96567374
TerrillT5 <t.thorne96@yahoo.com>
2018-07-10 18:21:26 -0400

    Added redux form

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import { reducer as reduxForm } from 'redux-form';
 import authReducer from './authReducer';
 
-// Wiring up redux 
+// Wiring up redux
 export default combineReducers ({
-  auth: authReducer
+  auth: authReducer,
+  form: reduxForm
 });
