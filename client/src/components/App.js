ea9e04e000c24bc17fb34fb766016caa47b2a32c
TerrillT5 <t.thorne96@yahoo.com>
2018-07-10 18:23:25 -0400

    imported new components to be used

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
-// BrowserRouter tells React Router how to behave
-// Looks at router & changes the components visible on the screen
-import { BrowserRouter, Route } from 'react-router-dom';
-import Landing from './Landing';
 import { connect } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 import * as actions from '../actions';
 
+// BrowserRouter tells React Router how to behave
+// Looks at router & changes the components visible on the screen
 import Header from './Header';
+import Landing from './Landing';
+import Dashboard from './Dashboard';
+import SurveyNew from './surveys/SurveyNew';
 
 import 'materialize-css/dist/css/materialize.min.css';
-
-const Dashboard = () => <h2>Dashboard</h2>
-const SurveyNew = () => <h2>SurveyNew</h2>
 
 class App extends Component {
   componentDidMount(props) {
